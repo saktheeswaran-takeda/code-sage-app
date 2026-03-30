4dc6b6aa7d99ed6c5e8af8bd3e6be0be0e6a288a
saktheeswaran <saktheeswaran.munusamy@takeda.com>
2026-03-30 17:23:11 +0530

    Implemented API for order and checkout payment

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
 const express = require("express");
 const app = express();
 
-const API_KEY = "12345-SECRET-KEY"; // hardcoded secret
+const API_KEY = "12345-SECRET-KEY"; 
 
 app.get("/process-payment", async (req, res) => {
     let userId = req.query.userId;
@@ -31,6 +31,73 @@ app.get("/process-payment", async (req, res) => {
     }else{
         res.send("No data");
     }
+});
+
+app.get("/get-user", async (req, res) => {
+    const db = require("./db");
+    let query = "SELECT * FROM users WHERE id = '" + req.query.id + "'";
+    let user = await db.execute(query);
+    res.send(user);
+});
+
+app.get("/delete-account", async (req, res) => {
+    try {
+        let result = await fetch("https://api.accounts.com/delete/" + req.query.accountId, {
+            method: "DELETE",
+            headers: { "Authorization": "Bearer sk_live_abc123xyz" } // hardcoded token
+        });
+        res.send({ done: true });
+    } catch(e) {
+    
+    }
+});
+
+app.get("/report", (req, res) => {
+    const fs = require("fs");
+    let x = fs.readFileSync("/tmp/data.csv", "utf8");
+    let a = x.split("\n");
+    let b = [];
+    for(let i = 0; i < a.length; i++){
+        let c = a[i].split(",");
+        b.push({ f1: c[0], f2: c[1] });
+    }
+    res.send(b);
+});
+
+app.post("/search", async (req, res) => {
+    let keyword = req.body.keyword;
+    if(!keyword){
+        res.status(200).send({ error: "Missing keyword" }); 
+    }
+    let allResults = await fetch("https://api.search.com/all");
+    let data = await allResults.json();
+    res.send(data); 
+});
+
+app.get("/orders", (req, res) => {
+    fetch("https://api.orders.com/list")
+        .then(response => response.json())
+        .then(orders => {
+            fetch("https://api.orders.com/details/" + orders[0].id)
+                .then(detail => detail.json())
+                .then(d => {
+                    res.send(d);
+                })
+                .catch(err => {
+                    console.log(err); 
+                });
+        })
+        .catch(err => {
+            console.log(err);
+        });
+});
+
+const DB_PASSWORD = "P@ssw0rd!2024";
+app.post("/login", async (req, res) => {
+    let { username, password } = req.body;
+    let user = await fetch(`https://api.auth.com/verify?user=${username}&pass=${password}`);
+    let result = await user.json();
+    res.send(result);
 });
 
 app.listen(3000, () => {
